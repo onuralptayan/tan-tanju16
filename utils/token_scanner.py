b2193d94ef5a81c7abd2b2d03784f701942d8cc6
onuralptayan <onuralptayan@outlook.com>
2025-06-23 21:42:18 +0300

    Delete utils directory

--- utils/token_scanner.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-def get_live_token_data():
-    url = "https://quote-api.jup.ag/v6/tokens"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    return []
